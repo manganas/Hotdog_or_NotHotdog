8569aa366bbc0be191f82a16298f0ec48f387912
manganas <manganas.ioannis@gmail.com>
2023-06-02 23:24:42 +0200

    added a basic CNN model

--- src/models/model.py
+++ src/models/model.py
@@ -1,5 +1,6 @@
 from torch import nn, Tensor
 import torch
+import torch.nn.functional as F
 
 
 class CNN_model(nn.Module):
@@ -10,17 +11,20 @@ class CNN_model(nn.Module):
     def __init__(self, in_channels: int, n_classes: int, h: int, w: int) -> None:
         super(CNN_model, self).__init__()
 
+        # Should we also add Recurrent layers?
+
         self.convolution_part = nn.Sequential(
-            nn.Conv2d(in_channels, 8),
+            nn.Conv2d(in_channels, 8, padding="same"),
             nn.LeakyReLU(),
-            nn.Conv2d(8, 16),
+            nn.Conv2d(8, 16, padding="same"),
             nn.LeakyReLU(),
             nn.BatchNorm2d(16),
-            nn.Conv2d(16, 32),
+            nn.Conv2d(16, 32, padding="same"),
             nn.LeakyReLU(),
-            nn.Conv2d(32, 32),
+            nn.Conv2d(32, 32, padding="same"),
             nn.LeakyReLU(),
             nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Flatten(),
         )
 
         x = torch.rand(1, in_channels, h, w)
@@ -35,8 +39,15 @@ class CNN_model(nn.Module):
         )
 
     def forward(self, x: Tensor) -> Tensor:
-        # Do not forget to assert and raise exception based on shape!
-        pass
+        x = self.convolution_part(x)
+        x = self.fully_connected(x)
+        x = F.log_softmax(x, dim=1)
+
+        assert len(x.shape) == 2, "Not a tensor of shape N,2"
+        assert x.shape[-1] == 2, "Not two class probs returned"
+
+        return x
 
     def get_output_conv_shape(self, x: Tensor) -> int:
-        pass
+        out = self.convolution_part(x)
+        return out.shape[1] * out.shape[2] * out.shape[3]
