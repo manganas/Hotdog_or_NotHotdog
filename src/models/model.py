c6c33fc284cb8e1268c18d045900bd2eec66b649
lefteriskat <lefteriskatiforis@gmail.com>
2023-06-06 18:16:38 +0200

    SImple final

--- src/models/model.py
+++ src/models/model.py
@@ -122,58 +122,7 @@ class CustomModelIma(nn.Module):
         x = self.fully_connected_part(x)
         return x
 
-class CNN_model(nn.Module):
-    """
-    Initial approach to a CNN for classification.
-    """
-
-    def __init__(self, in_channels: int, n_classes: int, h: int, w: int) -> None:
-        super(CNN_model, self).__init__()
-
-        # Should we also add Recurrent layers?
-
-        kernel_size = 3
-
-        self.convolution_part = nn.Sequential(
-            nn.Conv2d(in_channels, 8, kernel_size, padding="same"),
-            nn.LeakyReLU(),
-            nn.Conv2d(8, 16, kernel_size, padding="same"),
-            nn.LeakyReLU(),
-            nn.BatchNorm2d(16),
-            nn.Conv2d(16, 32, kernel_size, padding="same"),
-            nn.LeakyReLU(),
-            nn.Conv2d(32, 32, kernel_size, padding="same"),
-            nn.LeakyReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2),
-        )
-
-        x = torch.rand(1, in_channels, h, w)
-        lin_in_dim = self.get_output_conv_shape(x)
-
-        self.fully_connected = nn.Sequential(
-            nn.Linear(lin_in_dim, 256),
-            nn.LeakyReLU(),
-            nn.Linear(256, 128),
-            nn.LeakyReLU(),
-            nn.Linear(128, n_classes),
-            nn.LogSoftmax(dim=1),  # I am using NLLLoss for training!
-        )
-
-    def forward(self, x: Tensor) -> Tensor:
-        x = self.convolution_part(x)
-        x = x.view(x.size(0), -1)
-        x = self.fully_connected(x)
-
-        assert len(x.shape) == 2, "Not an array type of tensor"
-        assert x.shape[-1] == 2, "Not two class probs returned"
-
-        return x
-
-    def get_output_conv_shape(self, x: Tensor) -> int:
-        out = self.convolution_part(x)
-        return out.shape[1] * out.shape[2] * out.shape[3]
-    
-    
+  
 class CNN_model(nn.Module):
     """
     Initial approach to a CNN for classification.
